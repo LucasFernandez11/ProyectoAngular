97403012365eb6c30d8999535b373a5f83fad1e9
Lucas Fernández <89951452+LucasFernandez11@users.noreply.github.com>
2022-05-04 09:29:57 -0300

    creando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ContentAreaComponent } from './content-area/content-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ToolbarComponent,
+    ContentAreaComponent
   ],
   imports: [
     BrowserModule
